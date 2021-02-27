86a57fc1007c492dbb827ef6c92763cd1ac2b27d
Peter <pdevito@gmail.com>
2021-02-26 20:19:06 -0500

    Add sum function

--- Ch4/sumOfRange.js
+++ Ch4/sumOfRange.js
@@ -2,15 +2,36 @@
 // and returns an array containing all the numbers from start up to (and including) end.
 // Next, write a sum function that takes an array of numbers and returns the sum of these numbers.
 
-function range(start, end) {
-   if (start >= end) {
+function range(start, end, step) {
+   // handle undefined [step] parameter
+   if (step === undefined) {
+      if (start > end) step = -1;
+      else step = 1;
+   }
+
+   // handle a zero step (this would result in a infinitely running program)
+   if (step == 0) {
+      console.log('A step of zero is invalid!');
+      return undefined;
+   }
+
+   if ((step > 0 && start >= end) || (step < 0 && start <= end)) {
       return [start];
    }
    else {
-      return [start].concat(range(start + 1, end));
+      return [start].concat(range(start + step, end, step));
    }
 }
 
+const sum = ([start, ...numbers]) => { return numbers.length == 0 ? start : start + sum(numbers); };
+
 console.log(range(1,5));
+console.log(sum(range(1,5)));
 console.log(range(5,5));
+console.log(sum(range(5,5)));
 console.log(range(6,5));
+console.log(sum(range(6,5)));
+console.log(range(4,5));
+console.log(sum(range(4,5)));
+console.log(sum([1,2,3,4]));
+console.log(sum([]));
