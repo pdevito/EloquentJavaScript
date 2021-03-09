c8a666722303a0e2ab33d4001735c73f30260dea
Peter <pdevito@gmail.com>
2021-03-08 21:51:36 -0500

    Added comparison of the two objects

--- Ch4/deepEqual.js
+++ Ch4/deepEqual.js
@@ -4,10 +4,19 @@ function deepEqual(ele1, ele2) {
       isEqual = undefined;
    } else if (null == ele1 && null == ele2) {
       isEqual = true;
-   } else if ((null == ele1 && null != ele2) || (null != ele1 && null == ele2)) {
+   } else if ((ele1 == null) || (ele2 == null)) {
       isEqual = false;
    } else if (typeof(ele1) == 'object' && typeof(ele2) == 'object') {
-
+      let ele1Keys = Object.keys(ele1);
+      let ele2Keys = Object.keys(ele2);
+      if (ele1Keys.length == ele2Keys.length) {
+         isEqual = true;
+         for (let key of ele1Keys) {
+            if (!isEqual || !deepEqual(ele1[key], ele2[key])) {
+               isEqual = false;
+            }
+         }
+      }
    } else {
       isEqual = ele1 === ele2;
    }
