a3f699c405edf11feedc152679ecd71cf47d26ac
Peter <pdevito@gmail.com>
2021-03-09 22:18:35 -0500

    Initial commit of flatten.js

---
+++ Ch5/flatten.js
@@ -0,0 +1,11 @@
+function reduce(array, combine, start) {
+   let current = start;
+   for (let element of array) {
+     current = combine(current, element);
+   }
+   return current;
+ }
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+// → [1, 2, 3, 4, 5, 6]
+console.log(reduce(arrays, (a,b) => [].concat(a,b), []));
