d77b7114f633a42e4e57fcd159c678cfb6651dbe
Peter <pdevito@gmail.com>
2021-03-17 07:10:58 -0400

    Cleaned up vec.js

--- Ch6/vec.js
+++ Ch6/vec.js
@@ -17,12 +17,6 @@ class Vec {
    }
 }
 
-
-// Give the Vec prototype two methods, plus and minus, that take another vector as a parameter and return a new vector that has the sum or difference of the two vectors’ (this and the parameter) x and y values.
-
-// Add a getter property length to the prototype that computes the length of the vector—that is, the distance of the point (x, y) from the origin (0, 0).
-
-
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
